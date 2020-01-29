88832fa9726afbbbb97892195b5cf99f01bfff5c
Jai Sharma <jai.p.sharma1@gmail.com>
2020-01-29 01:05:49 +0000

    Added most of sixteenNine graphic

---
+++ src/graphics/sixteenNine/index.js
@@ -0,0 +1,56 @@
+import m from 'mithril';
+import { row, column } from '../common.css';
+import {
+  container, game, camera, sponsors, sponsorsInset, timer, timerText,
+  timerEstimate, runDetails, runGame, runMoreDetails, playerDetails,
+} from './sixteenNine.css';
+
+const timerRep = window.NodeCG.Replicant('timer', 'nodecg-speedcontrol');
+const runRep = window.NodeCG.Replicant('runDataActiveRun', 'nodecg-speedcontrol');
+
+const blankRun = {
+  game: 'Some Cool Game',
+  system: 'Arch Linux',
+  release: '1996',
+  category: 'Nipple%',
+  estimate: '00:00:00',
+  teams: [
+    {
+      players: [
+        { name: 'jai_' },
+      ],
+    },
+  ],
+};
+
+const safeRun = () => (runRep.value || blankRun);
+
+class SixteenNineComponent {
+  view() {
+    return m('div', { class: container },
+      m('div', { class: game }),
+      m('div', { class: camera }),
+      m('div', { class: `${column} ${sponsors}` },
+        m('div', { class: sponsorsInset })),
+      m('div', { class: `${column} ${timer}` },
+        m('div', { class: timerText }, timerRep.value.time),
+        m('div', { class: timerEstimate }, `Estimate: ${safeRun().estimate}`)),
+      m('div', { class: `${column} ${runDetails}` },
+        m('div', { class: runGame }, safeRun().game),
+        m('div', { class: `${row} ${runMoreDetails}` },
+          m('div', safeRun().system),
+          m('span', '·'),
+          m('div', safeRun().release),
+          m('span', '·'),
+          m('div', safeRun().category))),
+      m('div', { class: `${column} ${playerDetails}` }));
+  }
+}
+
+window.NodeCG.waitForReplicants(timerRep, runRep).then(() => {
+  m.mount(document.body, SixteenNineComponent);
+});
+
+
+timerRep.on('change', () => { m.redraw(); });
+runRep.on('change', () => { m.redraw(); });
