dd3cb58411b9e7890b60a85e7d5e5cd602918259
Jai Sharma <jai@jai.moe>
2020-02-01 11:40:52 +0000

    More break screen improvments

--- src/graphics/break/index.js
+++ src/graphics/break/index.js
@@ -1,23 +1,31 @@
 import m from 'mithril';
 import gsap from 'gsap';
-import { graphic, verticalSpacer } from '../common.css';
+import { graphic, horizontalSpacer, verticalSpacer } from '../common.css';
 import {
   container, breakContent, wasdIso, subtitle, cam, live, nowPlaying, spotify,
   songDetails, challenge, challengeName, challengeBar, challengeAmount,
-  challengeProgress, // nextRunsnext, RunItem,
+  challengeProgress, nextRunContainer, nextRunTop, nextRunBottom, label,
 } from './styles.css';
 
 const songRep = window.NodeCG.Replicant('currentSong', 'ncg-spotify');
 const challengeRep = window.NodeCG.Replicant('challenges', 'nodecg-tiltify');
+const runActiveRep = window.NodeCG.Replicant('runDataActiveRun', 'nodecg-speedcontrol');
+const runArrayRep = window.NodeCG.Replicant('runDataArray', 'nodecg-speedcontrol');
 
-/*
 class NextRunsComponent {
   view() {
-    return m('div', { class: nextRuns },
-      m('div', { class: nextRunItem }, 'Furi'));
+    const maxRunIndex = runArrayRep.value.length - 1;
+    const activeRunIndex = runArrayRep.value.findIndex((r) => r.id === runActiveRep.value.id);
+
+    const nextRun = (activeRunIndex === maxRunIndex)
+      ? { game: 'End of Stream', category: 'Thanks for watching', estimate: 'See you next time!' }
+      : runArrayRep.value[activeRunIndex + 1];
+
+    return m('div', { class: nextRunContainer },
+      m('div', { class: nextRunTop }, nextRun.game),
+      m('div', { class: nextRunBottom }, `${nextRun.category} - EST: ${nextRun.estimate}`));
   }
 }
-*/
 
 class DonationChallengeComponent {
   view(vnode) {
@@ -69,12 +77,17 @@ class BreakComponent {
           m('div', { class: songDetails }, `${songRep.value.name} - ${songRep.value.artist}`))),
       m('div', { class: verticalSpacer }),
       m('div', { class: breakContent },
+        m('div', { class: label }, 'Up Next:'),
+        m(NextRunsComponent),
+        m('div', { class: horizontalSpacer }),
+        m('div', { class: label }, 'Donation challenges:'),
         m(ChallengesComponent)));
   }
 }
 
-window.NodeCG.waitForReplicants(songRep, challengeRep).then(() => {
+window.NodeCG.waitForReplicants(songRep, challengeRep, runArrayRep, runActiveRep).then(() => {
   m.mount(document.body, BreakComponent);
 });
 
 songRep.on('change', () => { m.redraw(); });
+runActiveRep.on('change', () => { m.redraw(); });
